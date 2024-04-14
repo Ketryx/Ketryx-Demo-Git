d7ac5896547e4a987a0268dd0f32736241f1c1c6
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-14 19:30:25 -0400

    update test item id

--- app/patient/page.test.tsx
+++ app/patient/page.test.tsx
@@ -4,7 +4,7 @@
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
-it("Test warning message @tests:KXITM09D4Q0FGST9BVA61D5HYS9TSP2", () => {
+it("Test warning message @tests:KXITM63AKBT9S4R91SBRXMACZV225Y5", () => {
   render(<Page />);
   expect(screen.getByRole("heading")).toHaveTextContent("Patient App");
 });
