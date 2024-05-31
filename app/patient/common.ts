34bee90548c0d664d6b869ef2f182af0625dd6d2
Gabriel Pascualy <gabrielp@ketryx.com>
2024-05-31 14:20:17 -0400

    Update common.ts

--- app/patient/common.ts
+++ app/patient/common.ts
@@ -1,11 +1,9 @@
 /**
- * When either of the following conditions is met, the system shall issue a warning notification to the user through an appropriate user interface:
- * a) The recommended insulin dose exceeds the predefined upper or lower limits.
- * b) The latest entered glucose reading is outdated according to the defined maximum allowable time interval.
+ * Check warning threshold for the sensor calibration procedure.
  *
- * @itemId:spec-dose-reading-warning
- * @itemHasParent:MR-97
- * @itemFulfills: MR-22
+ * @itemId:reading-warning
+ * @itemHasParent:MR-38
+ * @itemFulfills: MR-21
  * @itemTitle:"Check warning threshold"
  *
  */
